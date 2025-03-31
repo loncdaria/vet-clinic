3527444234d9b24917e1892899873d417eab0166
loncdaria <loncdaria@gmail.com>
2025-03-31 16:39:48 +0200

    Dodane logowanie + migracja

--- main.py
+++ main.py
@@ -1,14 +1,64 @@
 from datetime import date, timedelta
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
+import hashlib
+import binascii
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_migrate import Migrate
 
-db = SQLAlchemy()
+
+
 app = Flask(__name__)
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.init_app(app)#nie wiem czy to potrzebne;wywala aplikacje
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"# tu nie wiem czy utworzyć plik config.cfg
+app.config['SECRET_KEY'] = 'Kiki'
+
 db.init_app(app)
 
+migrate = Migrate(app, db)
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(100))
+    first_name = db.Column(db.String(55))
+    last_name = db.Column(db.String(55))
+
+    def __repr__(self):
+        return ('User: {},{}'.format(self.name))
+    
+    def get_hash_password(self):
+        """Hash a password for storing"""
+        os_urandom_static=b'ID_\x03\x1e\xdd}Ae\x15\x93\xc5\xfe\\\x00o\xa5u+7\xfd\xdf\xf7\xbcN\x84:\xa6\xaf\x0c\x95\x0fK\x94\x06'
+        salt = hashlib.sha256(os_urandom_static).hexdigest().encode('ascii')
+        pwdhash = hashlib.pbkdf2_hmac('sha512', self.password.encode('utf-8'), salt, 100000)
+        pwdhash = binascii.hexlify(pwdhash)
+        return (salt + pwdhash).decode('ascii')
+    
+    def verify_password(self, stored_password, provided_password):
+        """Verify a stored password against one provided by user."""
+        salt = stored_password[:64]
+        stored_password = stored_password[64:]
+        pwdhash = hashlib.pbkdf2_hmac('sha512', provided_password.encode('utf-8'), salt.encode('ascii'), 100000)
+        pwdhash = binascii.hexlify(pwdhash).decode('ascii')
+        return pwdhash == stored_password
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.filter(User.id == id).first()
+
+class LoginForm(FlaskForm):
+    name=StringField('User name')
+    password=PasswordField('Password')
+    remember=BooleanField('Remember me')
 
 class Appointment(db.Model):
     __tablename__ = 'appointment'
@@ -43,10 +93,38 @@ class Vet(db.Model):
     mail = db.Column(db.String(255), nullable=False)
     birthDate = db.Column(db.Integer(), nullable=False)
 
+@app.route('/init')
+def init():
+    db.create_all()
+    
+    admin=User.query.filter(User.name=='admin').first()
+    if admin== None:
+        admin=User(id=1,name='admin',password=User.get_hash_password('admin'), first_name='admin', last_name='admin')
+        db.session.add(admin)
+        db.session.commit()
+    return 'OK'
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form=LoginForm()
+
+    if form.validate_on_submit():
+         user=User.query.filter(User.name==form.name.data).first()
+         if user != None and user.verify_password(user.password, form.password.data):
+            login_user(user)
+            return 'ok'
+    return render_template('login.html', form=form)
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return "wylogowano"
+    
 
 
 @app.route("/all/")
